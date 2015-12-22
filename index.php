a2810845b76f0a12427aea58350cdb9eb924bad3
Artur <arttigranyan@gmail.com>
2015-12-22 13:11:26 +0400

    upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+//require_once __DIR__ . '/vendor/autoload.php';
+//
+//$fb = new Facebook\Facebook([
+//    'app_id' => '{1673977016172871}',
+//    'app_secret' => '{ef4b01825ce2472335dd355812f770c1}',
+//    'default_graph_version' => 'v2.2',
+//]);
+
+echo "Hello";
