7d8b2bb9f0c091af4a9b1dd6071b1f7f9723dabd
Artur <arttigranyan@gmail.com>
2015-12-23 11:00:45 +0400

    newdupload

--- index.php
+++ index.php
@@ -5,14 +5,17 @@ session_start();
 
 
 $fb = new Facebook\Facebook([
-    'app_id' => '1673977016172871',
-    'app_secret' => 'ef4b01825ce2472335dd355812f770c1',
+    'app_id' => '495125297340244',
+    'app_secret' => 'c8fec76a911a11491b893e079bb91779',
     'default_graph_version' => 'v2.5',
 ]);
 
 $helper = $fb->getRedirectLoginHelper();
 
 $permissions = ['email']; // optional
+
+echo "Hello";
+
 
 try {
     if (isset($_SESSION['facebook_access_token'])) {
@@ -57,6 +60,7 @@ if (isset($accessToken)) {
 
     // getting basic info about user
     try {
+
         $profile_request = $fb->get('/me?fields=name,first_name,last_name,email');
         $profile = $profile_request->getGraphNode()->asArray();
     } catch(Facebook\Exceptions\FacebookResponseException $e) {
@@ -81,4 +85,50 @@ if (isset($accessToken)) {
     $loginUrl = $helper->getLoginUrl('https://fbappi.herokuapp.com/index.php', $permissions);
     echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
 }
- 
+
+?>
+
+<h1 id="fb-welcome"></h1>
+
+<script>
+    function onLogin(response) {
+        if (response.status == 'connected') {
+            FB.api('/me?fields=first_name', function(data) {
+                var welcomeBlock = document.getElementById('fb-welcome');
+                welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
+            });
+        }
+    }
+    FB.getLoginStatus(function(response) {
+        // Check login status on load, and if the user is
+        // already logged in, go directly to the welcome message.
+        if (response.status == 'connected') {
+            onLogin(response);
+        } else {
+            // Otherwise, show Login dialog first.
+            FB.login(function(response) {
+                onLogin(response);
+            }, {scope: 'user_friends, email'});
+        }
+    });
+</script>
+
+<script>
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '495125297340244',
+      xfbml      : true,
+      version    : 'v2.5'
+    });
+
+    // ADD ADDITIONAL FACEBOOK CODE HERE
+  };
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
