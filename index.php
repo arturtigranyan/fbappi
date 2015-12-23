78228a3273c10229b227ab48aa4cd6922961584f
Artur <arttigranyan@gmail.com>
2015-12-23 14:12:47 +0400

    fbn

--- index.php
+++ index.php
@@ -11,10 +11,8 @@ $fb = new Facebook\Facebook([
 
 
 
-$helper = $fb->getRedirectLoginHelper();
-
-$permissions = ['email']; // optional
-
+$helper = $fb->getCanvasHelper();
+$permissions = ['email', 'publish_actions']; // optional
 
 try {
     if (isset($_SESSION['facebook_access_token'])) {
@@ -36,50 +34,53 @@ if (isset($accessToken)) {
     if (isset($_SESSION['facebook_access_token'])) {
         $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
     } else {
-        // getting short-lived access token
         $_SESSION['facebook_access_token'] = (string) $accessToken;
-
         // OAuth 2.0 client handler
         $oAuth2Client = $fb->getOAuth2Client();
-
         // Exchanges a short-lived access token for a long-lived one
         $longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
-
         $_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
-
-        // setting default access token to be used in script
         $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
     }
 
-    // redirect the user back to the same page if it has "code" GET variable
-    if (isset($_GET['code'])) {
-        header('Location: ./');
-    }
-
-    // getting basic info about user
+    // validating the access token
     try {
-
-        $profile_request = $fb->get('/me?fields=name,first_name,last_name,email');
-        $profile = $profile_request->getGraphNode()->asArray();
+        $request = $fb->get('/me');
     } catch(Facebook\Exceptions\FacebookResponseException $e) {
         // When Graph returns an error
-        echo 'Graph returned an error: ' . $e->getMessage();
-        session_destroy();
-        // redirecting user back to app login page
-        header("Location: ./");
-        exit;
+        if ($e->getCode() == 190) {
+            unset($_SESSION['facebook_access_token']);
+            $helper = $fb->getRedirectLoginHelper();
+            $loginUrl = $helper->getLoginUrl('https://fbappi.herokuapp.com/index.php', $permissions);
+            echo "<script>window.top.location.href='".$loginUrl."'</script>";
+            exit;
+        }
     } catch(Facebook\Exceptions\FacebookSDKException $e) {
         // When validation fails or other local issues
         echo 'Facebook SDK returned an error: ' . $e->getMessage();
         exit;
     }
 
-    // printing $profile array on the screen which holds the basic info about user
-    print_r($profile);
-
-    // Now you can redirect to another page and use the access token from $_SESSION['facebook_access_token']
+    // posting on user timeline using publish_actins permission
+    try {
+        // message must come from the user-end
+        $data = ['message' => 'testing...'];
+        $request = $fb->post('/me/feed', $data);
+        $response = $request->getGraphEdge()->asArray;
+    } catch(Facebook\Exceptions\FacebookResponseException $e) {
+        // When Graph returns an error
+        echo 'Graph returned an error: ' . $e->getMessage();
+        exit;
+    } catch(Facebook\Exceptions\FacebookSDKException $e) {
+        // When validation fails or other local issues
+        echo 'Facebook SDK returned an error: ' . $e->getMessage();
+        exit;
+    }
+    echo $response['id'];
+    // Now you can redirect to another page and use the
+    // access token from $_SESSION['facebook_access_token']
 } else {
-    // replace your website URL same as added in the developers.facebook.com/apps e.g. if you used http instead of https and you used non-www version or www version of your website then you must add the same here
+    $helper = $fb->getRedirectLoginHelper();
     $loginUrl = $helper->getLoginUrl('https://fbappi.herokuapp.com/index.php', $permissions);
-    echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
+    echo "<script>window.top.location.href='".$loginUrl."'</script>";
 }
