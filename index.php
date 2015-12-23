d39acae7ac255ac4764fd3cf8511c7df5986ef15
Artur <arttigranyan@gmail.com>
2015-12-23 13:52:06 +0400

    newapp

--- index.php
+++ index.php
@@ -11,16 +11,11 @@ $fb = new Facebook\Facebook([
 
 $helper = $fb->getCanvasHelper();
 
-
-
 $permissions = ['email', 'publish_actions']; // optional
 
 try {
-    echo "Hello";
-    die;
     if (isset($_SESSION['facebook_access_token'])) {
         $accessToken = $_SESSION['facebook_access_token'];
-
     } else {
         $accessToken = $helper->getAccessToken();
     }
@@ -91,48 +86,3 @@ if (isset($accessToken)) {
 }
 
 ?>
-
-<h1 id="fb-welcome"></h1>
-
-<script>
-    function onLogin(response) {
-        if (response.status == 'connected') {
-            FB.api('/me?fields=first_name', function(data) {
-                var welcomeBlock = document.getElementById('fb-welcome');
-                welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
-            });
-        }
-    }
-    FB.getLoginStatus(function(response) {
-        // Check login status on load, and if the user is
-        // already logged in, go directly to the welcome message.
-        if (response.status == 'connected') {
-            onLogin(response);
-        } else {
-            // Otherwise, show Login dialog first.
-            FB.login(function(response) {
-                onLogin(response);
-            }, {scope: 'user_friends, email'});
-        }
-    });
-</script>
-
-<script>
-window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '495125297340244',
-      xfbml      : true,
-      version    : 'v2.5'
-    });
-
-    // ADD ADDITIONAL FACEBOOK CODE HERE
-  };
-
-(function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>
