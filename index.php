6e5a3f2f00208d2e7b94f294e38d05f716850f58
Artur <arttigranyan@gmail.com>
2015-12-23 16:17:18 +0400

    fgb

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+
 require_once __DIR__ . '/vendor/autoload.php';
 session_start();
 
@@ -11,6 +12,8 @@ $fb = new Facebook\Facebook([
 
 $helper = $fb->getCanvasHelper();
 $permissions = ['email', 'publish_actions']; // optional
+
+
 try {
     if (isset($_SESSION['facebook_access_token'])) {
         $accessToken = $_SESSION['facebook_access_token'];
@@ -26,46 +29,42 @@ try {
     echo 'Facebook SDK returned an error: ' . $e->getMessage();
     exit;
 }
+
 if (isset($accessToken)) {
     if (isset($_SESSION['facebook_access_token'])) {
         $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
     } else {
+        // getting short-lived access token
         $_SESSION['facebook_access_token'] = (string) $accessToken;
+
         // OAuth 2.0 client handler
         $oAuth2Client = $fb->getOAuth2Client();
+
         // Exchanges a short-lived access token for a long-lived one
         $longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
+
         $_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
+
+        // setting default access token to be used in script
         $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
     }
 
-    // validating the access token
-    try {
-        $request = $fb->get('/me');
-    } catch(Facebook\Exceptions\FacebookResponseException $e) {
-        // When Graph returns an error
-        if ($e->getCode() == 190) {
-            unset($_SESSION['facebook_access_token']);
-            $helper = $fb->getRedirectLoginHelper();
-//            $loginUrl = $helper->getLoginUrl('https://fbappi.herokuapp.com/index.php', $permissions);
-            echo "<script>window.top.location.href='".$loginUrl."'</script>";
-            exit;
-        }
-    } catch(Facebook\Exceptions\FacebookSDKException $e) {
-        // When validation fails or other local issues
-        echo 'Facebook SDK returned an error: ' . $e->getMessage();
-        exit;
+    // redirect the user back to the same page if it has "code" GET variable
+    if (isset($_GET['code'])) {
+        header('Location: ./');
     }
 
-    // posting on user timeline using publish_actins permission
+    // getting basic info about user
     try {
-        // message must come from the user-end
-        $data = ['message' => 'testing...'];
-        $request = $fb->post('/me/feed', $data);
-        $response = $request->getGraphEdge()->asArray;
+
+        $profile_request = $fb->get('/me?fields=name,first_name,last_name,email');
+        $profile = $profile_request->getGraphNode()->asArray();
     } catch(Facebook\Exceptions\FacebookResponseException $e) {
         // When Graph returns an error
         echo 'Graph returned an error: ' . $e->getMessage();
+        session_destroy();
+        // redirecting user back to app login page
+        header("Location: ./");
         exit;
     } catch(Facebook\Exceptions\FacebookSDKException $e) {
         // When validation fails or other local issues
@@ -73,12 +72,12 @@ if (isset($accessToken)) {
         exit;
     }
 
-    echo $response['id'];
+    // printing $profile array on the screen which holds the basic info about user
+    print_r($profile);
 
-    // Now you can redirect to another page and use the
-    // access token from $_SESSION['facebook_access_token']
+    // Now you can redirect to another page and use the access token from $_SESSION['facebook_access_token']
 } else {
-    $helper = $fb->getRedirectLoginHelper();
+    // replace your website URL same as added in the developers.facebook.com/apps e.g. if you used http instead of https and you used non-www version or www version of your website then you must add the same here
     $loginUrl = $helper->getLoginUrl('https://fbappi.herokuapp.com/index.php', $permissions);
-    echo "<script>window.top.location.href='".$loginUrl."'</script>";
+    echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
 }
