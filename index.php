7390e2121b92e98a4f67a8032d5503072c998d74
Artur <arttigranyan@gmail.com>
2015-12-24 10:44:33 +0400

    comurl

--- index.php
+++ index.php
@@ -1,6 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId      : '495125297340244',
+                xfbml      : true,
+                version    : 'v2.5'
+            });
+
+            // ADD ADDITIONAL FACEBOOK CODE HERE
+        };
+
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
+
+    <h1 id="fb-welcome">Facebook Login</h1>
+
+    <script>
+        function onLogin(response) {
+            if (response.status == 'connected') {
+                FB.api('/me?fields=first_name', function(data) {
+                    var welcomeBlock = document.getElementById('fb-welcome');
+                    welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
+                });
+            }
+        }
+
+        FB.getLoginStatus(function(response) {
+            // Check login status on load, and if the user is
+            // already logged in, go directly to the welcome message.
+            if (response.status == 'connected') {
+                onLogin(response);
+            } else {
+                // Otherwise, show Login dialog first.
+                FB.login(function(response) {
+                    onLogin(response);
+                }, {scope: 'user_friends, email'});
+            }
+        });
+    </script>
+
+
+</body>
+</html>
+
+
+
 <?php
 
-
+/*
 require_once __DIR__ . '/vendor/autoload.php';
 session_start();
 
@@ -17,139 +76,4 @@ $permissions = ['email']; // Optional permissions
 $loginUrl = $helper->getLoginUrl('https://fbappi.herokuapp.com/index.php', $permissions);
 
 echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
-
-$helper = $fb->getRedirectLoginHelper();
-
-try {
-    $accessToken = $helper->getAccessToken();
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
-    // When Graph returns an error
-    echo 'Graph returned an error: ' . $e->getMessage();
-    exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-    // When validation fails or other local issues
-    echo 'Facebook SDK returned an error: ' . $e->getMessage();
-    exit;
-}
-
-if (! isset($accessToken)) {
-    if ($helper->getError()) {
-        header('HTTP/1.0 401 Unauthorized');
-        echo "Error: " . $helper->getError() . "\n";
-        echo "Error Code: " . $helper->getErrorCode() . "\n";
-        echo "Error Reason: " . $helper->getErrorReason() . "\n";
-        echo "Error Description: " . $helper->getErrorDescription() . "\n";
-    } else {
-        header('HTTP/1.0 400 Bad Request');
-        echo 'Bad request';
-    }
-    exit;
-}
-
-// Logged in
-echo '<h3>Access Token</h3>';
-var_dump($accessToken->getValue());
-
-// The OAuth 2.0 client handler helps us manage access tokens
-$oAuth2Client = $fb->getOAuth2Client();
-
-// Get the access token metadata from /debug_token
-$tokenMetadata = $oAuth2Client->debugToken($accessToken);
-echo '<h3>Metadata</h3>';
-var_dump($tokenMetadata);
-
-// Validation (these will throw FacebookSDKException's when they fail)
-$tokenMetadata->validateAppId($config['app_id']);
-// If you know the user ID this access token belongs to, you can validate it here
-//$tokenMetadata->validateUserId('123');
-$tokenMetadata->validateExpiration();
-
-if (! $accessToken->isLongLived()) {
-    // Exchanges a short-lived access token for a long-lived one
-    try {
-        $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
-    } catch (Facebook\Exceptions\FacebookSDKException $e) {
-        echo "<p>Error getting long-lived access token: " . $helper->getMessage() . "</p>\n\n";
-        exit;
-    }
-
-    echo '<h3>Long-lived</h3>';
-    var_dump($accessToken->getValue());
-}
-
-$_SESSION['fb_access_token'] = (string) $accessToken;
-
-
-/*
-$helper = $fb->getCanvasHelper();
-
-$permissions = ['email']; // optional
-
-
-try {
-    if (isset($_SESSION['facebook_access_token'])) {
-        $accessToken = $_SESSION['facebook_access_token'];
-    } else {
-        $accessToken = $helper->getAccessToken();
-    }
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
-    // When Graph returns an error
-    echo 'Graph returned an error: ' . $e->getMessage();
-    exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-    // When validation fails or other local issues
-    echo 'Facebook SDK returned an error: ' . $e->getMessage();
-    exit;
-}
-
-if (isset($accessToken)) {
-    if (isset($_SESSION['facebook_access_token'])) {
-        $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-    } else {
-        // getting short-lived access token
-        $_SESSION['facebook_access_token'] = (string) $accessToken;
-
-        // OAuth 2.0 client handler
-        $oAuth2Client = $fb->getOAuth2Client();
-
-        // Exchanges a short-lived access token for a long-lived one
-        $longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
-
-        $_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
-
-        // setting default access token to be used in script
-        $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-    }
-
-    // redirect the user back to the same page if it has "code" GET variable
-    if (isset($_GET['code'])) {
-        header('Location: ./');
-    }
-
-    // getting basic info about user
-    try {
-
-        $profile_request = $fb->get('/me?fields=name,first_name,last_name,email');
-        $profile = $profile_request->getGraphNode()->asArray();
-    } catch(Facebook\Exceptions\FacebookResponseException $e) {
-        // When Graph returns an error
-        echo 'Graph returned an error: ' . $e->getMessage();
-        session_destroy();
-        // redirecting user back to app login page
-        header("Location: ./");
-        exit;
-    } catch(Facebook\Exceptions\FacebookSDKException $e) {
-        // When validation fails or other local issues
-        echo 'Facebook SDK returned an error: ' . $e->getMessage();
-        exit;
-    }
-
-    // printing $profile array on the screen which holds the basic info about user
-    print_r($profile);
-
-    // Now you can redirect to another page and use the access token from $_SESSION['facebook_access_token']
-} else {
-    // replace your website URL same as added in the developers.facebook.com/apps e.g. if you used http instead of https and you used non-www version or www version of your website then you must add the same here
-    $loginUrl = $helper->getLoginUrl('https://fbappi.herokuapp.com/index.php', $permissions);
-    echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
-}
+?>
