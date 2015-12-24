836d758adc5573061266c9986d75018f0ebddd61
Artur <arttigranyan@gmail.com>
2015-12-24 12:05:24 +0400

    cn

--- index.php
+++ index.php
@@ -10,8 +10,7 @@ $fb = new Facebook\Facebook([
 ]);
 
 $helper = $fb->getCanvasHelper();
-
-
+$permissions = ['email', 'publish_actions']; // optional
 try {
     if (isset($_SESSION['facebook_access_token'])) {
         $accessToken = $_SESSION['facebook_access_token'];
@@ -36,8 +35,26 @@ if (isset($accessToken)) {
         $oAuth2Client = $fb->getOAuth2Client();
         // Exchanges a short-lived access token for a long-lived one
         $longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
-
+        $_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
         $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+    }
+
+    // validating the access token
+    try {
+        $request = $fb->get('/me');
+    } catch(Facebook\Exceptions\FacebookResponseException $e) {
+        // When Graph returns an error
+        if ($e->getCode() == 190) {
+            unset($_SESSION['facebook_access_token']);
+            $helper = $fb->getRedirectLoginHelper();
+            $loginUrl = $helper->getLoginUrl('https://apps.facebook.com/newfbappi/', $permissions);
+            echo "<script>window.top.location.href='".$loginUrl."'</script>";
+            exit;
+        }
+    } catch(Facebook\Exceptions\FacebookSDKException $e) {
+        // When validation fails or other local issues
+        echo 'Facebook SDK returned an error: ' . $e->getMessage();
+        exit;
     }
 
     // posting on user timeline using publish_actins permission
@@ -45,13 +62,10 @@ if (isset($accessToken)) {
         // message must come from the user-end
         $data = ['message' => 'testing...'];
         $request = $fb->post('/me/feed', $data);
-//        $response = $request->getGraphEdge()->asArray;
         $response = $request->getGraphUser();
     } catch(Facebook\Exceptions\FacebookResponseException $e) {
         // When Graph returns an error
         echo 'Graph returned an error: ' . $e->getMessage();
-        unset($_SESSION['facebook_access_token']);
-        echo "<script>window.top.location.href='https://apps.facebook.com/newfbappi/'</script>";
         exit;
     } catch(Facebook\Exceptions\FacebookSDKException $e) {
         // When validation fails or other local issues
@@ -65,7 +79,6 @@ if (isset($accessToken)) {
     // access token from $_SESSION['facebook_access_token']
 } else {
     $helper = $fb->getRedirectLoginHelper();
-    $permission = ['email', 'publish_actions'];
     $loginUrl = $helper->getLoginUrl('https://apps.facebook.com/newfbappi/', $permissions);
     echo "<script>window.top.location.href='".$loginUrl."'</script>";
 }
